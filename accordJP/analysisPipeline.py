9e3509cbcaf893e2494725887de7ac52e394e8d9
2waybene <jianying.li@gmail.com>
2020-04-27 14:21:04 -0400

    something

--- accordJP/analysisPipeline.py
+++ accordJP/analysisPipeline.py
@@ -1,4 +1,49 @@
 
 
-def launchModelStep1 (filepath):
-    print ("hello world\t" + str(filepath))
+
+
+def launchModelStep (filepath, phenotype = "pheno_data.txt"):
+    print ("****** Begin JOB:' " + str(filepath) + "'")
+
+    #print 'Number of arguments', len(sys.argv), 'arguments.'
+    #print 'Argument List:', str(sys.argv)
+
+
+    #for path in filepath :
+    print ('*************************************')
+    print ('Current working path:', str(filepath))
+
+    ## Create system command
+        #cmd = ' '.join(('sbatch -p standard -o '+path+'/model_setup_step1.out ./bin/model_setup_step1.sh'), str(path) , str(phenotype))
+
+    cmd = 'hello world'
+    cmd = "sbatch -p standard -o " + filepath + "/model_setup_step1.out ./bin/model_setup_step1.sh" + filepath +  str(phenotype)
+    print (cmd)
+    print ("Launching model setup step 1:" +  cmd)
+
+        #dout=log_file,stderr=logerr_file)
+
+    print ("*************************************")
+    print ("******* End JOB: model step 1")
+    print ("*************************************\n")
+
+
+
+
+def launchModelStep1 (root, filepath, phenotype = "pheno_data.txt"):
+    print ("****** Begin JOB:' " + str(filepath) + "'")
+
+    #for path in filepath :
+    print ('*************************************')
+    print ('Current working path:', str(filepath))
+
+    ## Create system command
+
+    path =  root + "/" + filepath
+    cmd = "sbatch -p standard -o " + path + "/model_setup_step1.out " + path + "/bin/model_setup_step1.sh " + path +  str(phenotype)
+    print (cmd)
+    print ("Launching model setup step 1:" +  cmd)
+
+    print ("*************************************")
+    print ("******* End JOB: model step 1")
+    print ("*************************************\n")
--- accordJP/cli.py
+++ accordJP/cli.py
@@ -11,8 +11,20 @@ def main(args=None):
 @click.argument('filename', type=click.Path(exists=True))
 def touch(filename):
     """Print FILENAME if the file exists."""
+    """Print FILENAME if the file exists."""
     click.echo(click.format_filename(filename))
     analysisPipeline.launchModelStep1(filename)
+
+@main.command()
+@click.argument('filename', type=click.Path(exists=True))
+@click.argument('analysisdir', type=str)
+def modelStep1(filename, analysisdir=None):
+    """Print FILENAME if the file exists."""
+
+    click.echo(click.format_filename(filename))
+    click.echo(analysisdir)
+    analysisPipeline.launchModelStep1(filename, analysisdir)
+
 
 
 @main.command()
