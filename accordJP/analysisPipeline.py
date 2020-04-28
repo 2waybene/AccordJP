39b7b6426f3447aec862e7ce22d0e50a07793848
2waybene <jianying.li@gmail.com>
2020-04-28 16:03:28 -0400

    new on mac

--- accordJP/analysisPipeline.py
+++ accordJP/analysisPipeline.py
@@ -7,61 +7,89 @@ import sys
 import shlex 
 import subprocess as sp
 
-def launchModelStep (filepath, phenotype = "pheno_data.txt"):
+def launchModelStep1 (filepath, phenotype = "pheno_data.txt"):
+
     print ("****** Begin JOB:' " + str(filepath) + "'")
-
-    #print 'Number of arguments', len(sys.argv), 'arguments.'
-    #print 'Argument List:', str(sys.argv)
-
 
     #for path in filepath :
     print ('*************************************')
-    print ('Current working path:', str(filepath))
+    print ('This is the working path entered from the user:', str(filepath))
 
     ## Create system command
 
-<<<<<<< HEAD
-    cmd = "sbatch -p standard -o " + filepath + "/model_setup_step1.out ./bin/model_setup_step1.sh  " + filepath + " " +   str(phenotype)
-=======
-    # cmd = 'srun --partition=bioinfo --cpus-per-task=8 -o RHTN_testRun/rhtn_combined/model_setup_step1.out ./bin/model_setup_step1.sh  RHTN_testRun/rhtn_combined pheno_data.txt'
+    # cmd = "sbatch -p standard -o " + filepath + "/model_setup_step1.out ./bin/model_setup_step1.sh  " + filepath + " " +   str(phenotype)
 
-<<<<<<< HEAD
     cmd = "srun --partition=bioinfo --cpus-per-task=8 -o  " + filepath + "/model_setup_step1.out ./bin/model_setup_step1.sh  " + filepath +  "  " + str(phenotype)
-   
-    ## cmd = "sbatch -p standard -o " + filepath + "/model_setup_step1.out ./bin/model_setup_step1.sh" + filepath +  str(phenotype)
-    print (cmd)
-    sp.call (cmd)
-=======
-    cmd = "srun --partition=bioinfo --cpus-per-task=8 -o  " + filepath + "/model_setup_step1.out ./bin/model_setup_step1.sh" + filepath +  str(phenotype)
-
-    # cmd = "sbatch -p standard -o " + filepath + "/model_setup_step1.out ./bin/model_setup_step1.sh" + filepath +  str(phenotype)
->>>>>>> 5298bed03e8244fc732fc6120d30ba8b1f0b366d
     print (cmd)
     sp.call(cmd,  shell=True)
->>>>>>> a8af152408a3fad88b3b0363dafd815c8a35f136
     print ("Launching model setup step 1:" +  cmd)
-
-        #dout=log_file,stderr=logerr_file)
-
-    print ("*************************************")
-    print ("******* End JOB: model step 1")
-    print ("*************************************\n")
+    print ("Check the job status with command: squeue ")
 
 
-def launchModelStep1 (root, filepath, phenotype = "pheno_data.txt"):
+def launchModelStep2 (filepath):
+
     print ("****** Begin JOB:' " + str(filepath) + "'")
 
     #for path in filepath :
     print ('*************************************')
-    print ('Current working path:', str(filepath))
+    print ('This is the working path entered from the user:', str(filepath))
+
+    ## Create system command
+    ## ('sbatch -p standard -o '+path+'/model_setup_step2.out ./bin/model_setup_step2.sh',path))
+    cmd = "srun --partition=bioinfo --cpus-per-task=8 -o  " + filepath + "/model_setup_step2.out ./bin/model_setup_step2.sh  " + filepath
+    print (cmd)
+    sp.call(cmd,  shell=True)
+
+    print ("Launching model setup step 2:" +  cmd)
+    print ("Check the job status with command: squeue ")
+
+
+def launchHeritability (filepath):
+
+    print ("****** Begin JOB:' " + str(filepath) + "'")
+    #for path in filepath :
+    print ('*************************************')
+    print ('This is the working path entered from the user:', str(filepath))
 
     ## Create system command
 
-    path =  root + "/" + filepath
-    cmd = "sbatch -p standard -o " + path + "/model_setup_step1.out " + path + "/bin/model_setup_step1.sh " + path +  str(phenotype)
+    # cmd = 'sbatch -p standard -o '+path+'/sbatch_logs/gcta.out ./bin/run_gcta.sh',path))
+    cmd = "srun --partition=bioinfo --cpus-per-task=8 -o  " + filepath + "/sbatch_logs/gcta.out  ./bin/run_gcta.sh  " + filepath
     print (cmd)
-    print ("Launching model setup step 1:" +  cmd)
-    sp.call(cmd)
-    print ("*************************************")
-    print ("******* End JOB: model step 1")
-    print ("*************************************\n")
+    sp.call(cmd,  shell=True)
+    print ("Launching launchHeritability step 1 of 3:" + cmd)
+    print ("Check the job status with command: squeue ")
+
+
+def genoCommondVarAnalysis (filepath):
+
+    print ("****** Begin JOB:' " + str(filepath) + "'")
+    #for path in filepath :
+    print ('*************************************')
+    print ('This is the working path entered from the user:', str(filepath))
+
+    ## Create system command
+
+    # cmd = 'sbatch -p standard -o '+path+'/sbatch_logs/gcta.out ./bin/run_gcta.sh',path))
+    cmd = "place holder"
+    print (cmd)
+    sp.call(cmd,  shell=True)
+    print ("Launching genotype common variant analysis  step 2 of 3:" + cmd)
+    print ("Check the job status with command: squeue ")
+
+
+def imputeCommondVarAnalysis (filepath):
+
+    print ("****** Begin JOB:' " + str(filepath) + "'")
+    #for path in filepath :
+    print ('*************************************')
+    print ('This is the working path entered from the user:', str(filepath))
+
+    ## Create system command
+
+    # cmd = 'sbatch -p standard -o '+path+'/sbatch_logs/gcta.out ./bin/run_gcta.sh',path))
+    cmd = "place holder"
+    print (cmd)
+    sp.call(cmd,  shell=True)
+    print ("Launching impute common variant analysis  step 3 of 3:" + cmd)
+    print ("Check the job status with command: squeue ")
--- accordJP/cli.py
+++ accordJP/cli.py
@@ -7,38 +7,51 @@ from . import  analysisPipeline
 def main(args=None):
     pass
 
-@main.command()
-@click.argument('filename', type=str)
-<<<<<<< HEAD
-#click.Path(exists=True))
-=======
->>>>>>> 5298bed03e8244fc732fc6120d30ba8b1f0b366d
-
-def touch(filename):
-    """Print FILENAME if the file exists."""
-    """Print FILENAME if the file exists."""
-    click.echo(click.format_filename(filename))
-    analysisPipeline.launchModelStep(filename)
 
 @main.command()
-@click.argument('filename', type=click.Path(exists=True))
-@click.argument('analysisdir', type=str)
-def modelStep1(filename, analysisdir=None):
-    """Print FILENAME if the file exists."""
+@click.argument('inputdir', type=click.Path(exists=True))
+@click.argument('phenodata', type=str)
+def modelStep1(inputdir, phenodata=None):
 
-    click.echo(click.format_filename(filename))
-    click.echo(analysisdir)
-    analysisPipeline.launchModelStep1(filename, analysisdir)
+    """
+
+    Run accordJP pipeline, starting launch model setup step 1
+
+    As of this moment, JYL -- FIXME
+
+    Make sure your phenotype data file is called '/home/accord/data/analysis/pheno_data.txt' before launching step 1
+    Update: File should be named "pheno_data_*****.txt" Step 1 will prompt user for pheno file name
+    Make sure you are in ~/accord/data/analysis when you start running this code
+
+    Print INPUTDIR if the directory exists.
+    Print PHENODATA from the input or use defaulty: pheno_data.txt
+    """
 
 
+    click.echo(click.format_filename(inputdir))
+    click.echo(phenodata)
+    analysisPipeline.launchModelStep1(inputdir, phenodata)
 
 @main.command()
-def initdb():
-    click.echo('Initialized the database')
+@click.argument('inputdir', type=click.Path(exists=True))
 
-@main.command()
-def dropdb():
-    click.echo('Dropped the database')
+def modelStep2(inputdir):
+
+    """
+
+    Run accordJP pipeline, starting launch model setup step 2
+
+    As of this moment, JYL -- FIXME
+
+    Print INPUTDIR if the directory exists.
+
+    """
+
+    click.echo(click.format_filename(inputdir))
+    analysisPipeline.launchModelStep2(inputdir)
+
+
+
 
 if __name__ == "__main__":
     main()
