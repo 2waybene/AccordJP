1c8608ad8d9c9050fade4ec46dccdc0a2c46a07b
Jianying Li <jianying.li@gmail.com>
2020-04-29 19:17:50 -0400

    Update cli.py

--- accordJP/cli.py
+++ accordJP/cli.py
@@ -2,6 +2,8 @@ import click
 import sys
 from . import  analysisPipeline
 
+"""Console script for accordJP."""
+
 
 @click.group()
 def main(args=None):
@@ -13,19 +15,19 @@ def main(args=None):
 @click.argument('phenodata', type=str)
 def modelStep1(inputdir, phenodata=None):
 
-    """
-
-    Run accordJP pipeline, starting launch model setup step 1
-
+    '''
+    Running accordJP pipeline, step 1:
+    launch model setup step 1
     As of this moment, JYL -- FIXME
-
+    Instruction from Dr. John House:
     Make sure your phenotype data file is called '/home/accord/data/analysis/pheno_data.txt' before launching step 1
     Update: File should be named "pheno_data_*****.txt" Step 1 will prompt user for pheno file name
     Make sure you are in ~/accord/data/analysis when you start running this code
-
     Print INPUTDIR if the directory exists.
     Print PHENODATA from the input or use defaulty: pheno_data.txt
-    """
+    '''
+
+
 
 
     click.echo(click.format_filename(inputdir))
@@ -34,22 +36,43 @@ def modelStep1(inputdir, phenodata=None):
 
 @main.command()
 @click.argument('inputdir', type=click.Path(exists=True))
-
 def modelStep2(inputdir):
 
     """
-
-    Run accordJP pipeline, starting launch model setup step 2
-
-    As of this moment, JYL -- FIXME
-
+    Running accordJP pipeline, step 2:
+    launch model setup step 2
+    User needs to provide the directory as the argument to kick off the analysis.
     Print INPUTDIR if the directory exists.
-
+    As of this moment, JYL -- FIXME
     """
-
     click.echo(click.format_filename(inputdir))
     analysisPipeline.launchModelStep2(inputdir)
 
+
+@main.command()
+@click.argument('inputdir', type=click.Path(exists=True))
+def heritability(inputdir):
+
+    """
+    Run accordJP pipeline,
+    starting heritability analysis.
+    As of this moment, JYL -- FIXME
+    Print INPUTDIR if the directory exists.
+    """
+    click.echo(click.format_filename(inputdir))
+    analysisPipeline.launchHeritability(inputdir)
+
+@main.command()
+@click.argument('inputdir', type=click.Path(exists=True))
+def heritability(inputdir):
+
+    """
+    Run accordJP pipeline, starting launch heritability test
+    As of this moment, JYL -- FIXME
+    Print INPUTDIR if the directory exists.
+    """
+    click.echo(click.format_filename(inputdir))
+    analysisPipeline.launchHeritability(inputdir)
 
 
 
